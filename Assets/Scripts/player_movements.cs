ee9fa5333256b8afec7a9cc63b169415311ef329
cwkchi5781 <friendsrock2004chi@gmail.com>
2020-06-30 20:11:26 -0700

    first commit

---
+++ Assets/Scripts/player_movements.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_movements : MonoBehaviour
+{
+    public float velocity = 10f;
+    public Rigidbody rb;
+    public int score;
+    public Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        setScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical"); 
+
+        Vector3 movement = new Vector3(moveX, 0.0f, moveZ);
+
+        rb.AddForce(movement * velocity);
+
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            score += 1;
+            setScoreText();
+            print("Score: " + score);
+            
+        }
+    }
+
+    void setScoreText(){
+        ScoreText.text = "Score: " + score.ToString();
+    }
+}
+
